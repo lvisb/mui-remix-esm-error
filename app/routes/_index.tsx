d08c7b01228762f0335bef18891cc73328f6a086
lvisb <luis.flashdev@gmail.com>
2024-12-20 17:01:24 -0300

    *

---
+++ app/routes/_index.tsx
@@ -0,0 +1,17 @@
+import { Close } from "@mui/icons-material";
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Close />
+    </div>
+  );
+}
